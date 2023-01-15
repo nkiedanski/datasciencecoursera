321efd0d838cf06479a6d77066e05fe909843766
Niki Kiedanski <nicole@192.168.1.2>
2023-01-15 19:10:54 -0300

    renaming file

--- Rprogramming_w4_Assignment_best.R
+++ Rprogramming_w4_Assignment_best.R
@@ -21,11 +21,11 @@ best <- function(state, outcome) {
     valid_states = levels(as.factor(data$State))
     
     if (identical(outcome,"heart attack")) {
-      mins = tapply(as.numeric(data$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), 
+      mins = tapply(as.numeric(data$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), 
              data$State, min, na.rm = TRUE, simplify = TRUE)
       lower = mins[state]
       bests = data[data$State == state & 
-                     data$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure==lower,
+                     data$Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack==lower,
                    "Hospital.Name"]
     }
     else if (identical(outcome,"heart failure")) {
@@ -48,8 +48,11 @@ best <- function(state, outcome) {
 }
 
 best("AZ", "heart attack")
+best("SC", "heart attack")
+best("NY", "pneumonia")
+best("AK", "pneumonia")
 
-
+# esta parte es para que tire error cuando no es un estado o no es un outcome
 
 #    if (!identical(outcome, "heart attack") | !identical(outcome, "heart failure")|
 #        !identical(outcome, "pneumonia")) {
@@ -58,3 +61,5 @@ best("AZ", "heart attack")
 #    if (!(state %in%valid_states)) {
 #      print("invalid state")
 #    }
+
+# falta la parte de que cuando son multiples, los ordene e imprima solo el primero
