c3127fd9122ee13ea57c96c0fa912bb2180cd620
Niki Kiedanski <nicole@192.168.1.2>
2023-01-15 17:08:25 -0300

    .

--- Rprogramming_w_corr.2.R
+++ Rprogramming_w_corr.2.R
@@ -1,4 +1,4 @@
-corr = function (directory, threshold=0, id = 1:323) {
+corr = function (directory, threshold=0, id = 1:332) {
   correlations = c()
   for (file_name in id) {
     corrected_id = sprintf("%03d", file_name)
@@ -7,21 +7,33 @@ corr = function (directory, threshold=0, id = 1:323) {
     obs = nrow(data[!is.na(data$sulfate) & !is.na(data$nitrate),])
     if (obs > threshold) {
       correlation = cor(data$sulfate, data$nitrate, use = "complete.obs")
+      correlations =  c(correlations, correlation)
     }
-    correlations =  c(correlations, correlation)
+    
   }
   correlations
 }
 
-cr <- corr("specdata", 150)
-head(cr)
-summary(cr)
-
-cr <- corr("specdata", 400)
+cr <- corr("specdata", threshold = 150)
 head(cr)
 
-cr = corr("specdata", 5000)
-head(cr)
+cr <- corr("specdata")                
+cr <- sort(cr)   
+RNGversion("3.5.1")
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
 
-cr = corr("specdata")
-head(cr)
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)    
+RNGversion("3.5.1")
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
