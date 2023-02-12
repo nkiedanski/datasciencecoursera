e5c26a880d901e6c388cfa525b558c44cf947bf2
Niki Kiedanski <nicole@Nikis-MacBook-Pro.local>
2023-02-12 14:47:44 -0300

    question 3, 4 and 5 included

--- get&clean_w4.R
+++ get&clean_w4.R
@@ -20,3 +20,27 @@ mean(data_product_clean$X.4)
 
 ################################################################################
 
+grep("^United",data_product_clean$X.3)
+
+# question 3: 1  6 32, entonces la respuesta es 3.
+
+################################################################################
+
+data_educational = read.csv("../datasciencecoursera/educational.csv", sep = ",", 
+                            header = TRUE)
+merged_data = merge(data_educational, data_product_clean, by.x = "CountryCode", by.y = "X")
+
+table(grepl("Fiscal year end: June", merged_data$Special.Notes))
+
+# question 4: 13
+
+################################################################################
+
+library(quantmod)
+
+start <- as.Date('2012-01-01')
+end <- as.Date('2012-12-31')
+
+getSymbols('AMZN', src = 'yahoo', from = start, to = end)
+
+# question 5: 250, 47
